1d7c3341551e3092317bb9a5b3df522a8e92d9a8
JKaypa <jose_kaypa@hotmail.com>
2025-01-08 18:37:17 -0500

    patient migration

---
+++ src/main/resources/db/migration/V5__create-table-patient.sql
@@ -0,0 +1,16 @@
+create table patient(
+
+    id bigint not null auto_increment,
+    name varchar(100) not null,
+    email varchar(100) not null unique,
+    id_number varchar(10) not null unique,
+    city varchar(100) not null,
+    district varchar(100) not null,
+    street varchar(100) not null,
+    number varchar(20),
+    complement varchar(100),
+    active tinyint not null,
+
+    primary key(id)
+
+);
