612c12f968b980200250a7540a2711c15b8a3ee9
JKaypa <jose_kaypa@hotmail.com>
2025-01-08 11:49:20 -0500

    user repository

--- src/main/java/med/voll/api/domain/users/UserRepository.java
+++ src/main/java/med/voll/api/domain/users/UserRepository.java
@@ -1,4 +1,8 @@
 package med.voll.api.domain.users;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
+    UserDetails findByUsername(String username);
 }
