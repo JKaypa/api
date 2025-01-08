8d712d51cb7e9cc0d66b1593272ce65e322839a5
JKaypa <jose_kaypa@hotmail.com>
2025-01-08 12:02:14 -0500

    refactor error handler

--- src/main/java/med/voll/api/infra/errors/ErrorHandler.java
+++ src/main/java/med/voll/api/infra/errors/ErrorHandler.java
@@ -1,6 +1,9 @@
-package med.voll.api.infra;
+package med.voll.api.infra.errors;
 
 import jakarta.persistence.EntityNotFoundException;
+
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,12 +14,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ErrorHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity handleError404(){
+    public ResponseEntity<Void> handleError404(){
         return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity handleError400(MethodArgumentNotValidException error){
+    public ResponseEntity<List<ValidationError>> handleError400(MethodArgumentNotValidException error){
         var errors = error.getFieldErrors().stream().map(ValidationError::new).toList();
         return ResponseEntity.badRequest().body(errors);
     }
