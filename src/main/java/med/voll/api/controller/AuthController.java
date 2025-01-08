bda9d8006fd46659205fd98c27d62bd9e12b84e5
JKaypa <jose_kaypa@hotmail.com>
2025-01-08 09:07:21 -0500

    + auth controller

--- src/main/java/med/voll/api/controller/AuthController.java
+++ src/main/java/med/voll/api/controller/AuthController.java
@@ -1,4 +1,38 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
+import med.voll.api.domain.users.UserRequestDto;
+import med.voll.api.domain.users.Users;
+import med.voll.api.infra.security.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
 public class AuthController {
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity<String> login(@RequestBody @Valid UserRequestDto user){
+        Authentication authToken = new UsernamePasswordAuthenticationToken(
+                user.username(),
+                user.password()
+        );
+
+        var authenticatedUser = authenticationManager.authenticate(authToken);
+        var JWTToken = tokenService.generateToken((Users) authenticatedUser.getPrincipal());
+
+        return ResponseEntity.ok(JWTToken);
+    }
 }
