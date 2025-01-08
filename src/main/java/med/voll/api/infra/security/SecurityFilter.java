b92a75919afa9784645e35478d28933730a1e92b
JKaypa <jose_kaypa@hotmail.com>
2025-01-08 12:03:22 -0500

    security filter

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -1,4 +1,36 @@
 package med.voll.api.infra.security;
 
-public class SecurityFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.users.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var bearerToken = request.getHeader("Authorization");
+        if( bearerToken != null) {
+            var token = bearerToken.substring(7);
+            var subject = tokenService.getSubject(token);
+            var authUser = userRepository.findByUsername(subject);
+            var auth = new UsernamePasswordAuthenticationToken(authUser, null, authUser.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        filterChain.doFilter(request, response);
+    }
 }
