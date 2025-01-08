d5bee4240cee69c95059b13541b742eace6e23c3
JKaypa <jose_kaypa@hotmail.com>
2025-01-08 12:03:34 -0500

    token service

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -1,4 +1,54 @@
 package med.voll.api.infra.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import med.voll.api.domain.users.UserRequestDto;
+import med.voll.api.domain.users.Users;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
 public class TokenService {
+    @Value("${SECRET}")
+    private String secret;
+
+    public String generateToken(Users user){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("vollmed")
+                    .withSubject(user.getUsername())
+                    .withClaim("id", user.getId())
+                    .withExpiresAt(generateExpirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException();
+        }
+    }
+
+    public String getSubject(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            DecodedJWT verifier = JWT.require(algorithm)
+                    .withIssuer("vollmed")
+                    .build()
+                    .verify(token);
+
+            return verifier.getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Invalid token verifier");
+        }
+    }
+
+    private Instant generateExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-05:00"));
+    }
 }
