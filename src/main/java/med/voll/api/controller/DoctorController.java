4456c3f5ed93dd3156ed6056f755eb3e1d00231b
JKaypa <jose_kaypa@hotmail.com>
2025-01-08 11:32:09 -0500

    doctor controller

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -2,13 +2,17 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.doctor.*;
+import med.voll.api.domain.doctor.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 
 @RestController
@@ -18,27 +22,37 @@ public class DoctorController {
     private DoctorRepository doctorRepository;
 
     @PostMapping
-    public void postDoctor(@RequestBody @Valid DoctorDto doctorDto){
+    public ResponseEntity<DoctorResponseDto> postDoctor(@RequestBody @Valid DoctorDto doctorDto, UriComponentsBuilder uriBuilder){
         var doctor = new Doctor(doctorDto);
-        doctorRepository.save(doctor);
+        var newDoctor = doctorRepository.save(doctor);
+        URI uri = uriBuilder.path("/doctor/{id}").buildAndExpand(newDoctor.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DoctorResponseDto(newDoctor));
     }
 
     @GetMapping
-    public Page<DoctorListDto> getDoctors(@PageableDefault(sort = "name") Pageable pagination){
-        return doctorRepository.findAllByActiveTrue(pagination).map(DoctorListDto::new);
+    public Page<DoctorResponseDto> getDoctors(@PageableDefault(sort = "name") Pageable pagination){
+        return doctorRepository.findAllByActiveTrue(pagination).map(DoctorResponseDto::new);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DoctorDetailDto> getDoctorById(@PathVariable Long id){
+        return ResponseEntity.ok(new DoctorDetailDto(doctorRepository.getReferenceById(id)));
     }
 
     @PutMapping("/{id}")
     @Transactional
-    public void updateDoctor(@RequestBody @Valid UpdateDoctorDto updateDoctorDto, @PathVariable Long id){
+    public ResponseEntity<DoctorResponseDto> updateDoctor(@RequestBody @Valid UpdateDoctorDto updateDoctorDto, @PathVariable Long id){
         var doctor = doctorRepository.getReferenceById(id);
         doctor.update(updateDoctorDto);
+        return ResponseEntity.ok(new DoctorResponseDto(doctor));
     }
 
     @DeleteMapping("/{id}")
+    @Secured("ROLE_admin")
     @Transactional
-    public ResponseEntity deleteDoctor(@PathVariable Long id){
+    public ResponseEntity<Void> deleteDoctor(@PathVariable Long id){
         doctorRepository.getReferenceById(id).logicDelete();
+        
         return ResponseEntity.noContent().build();
     }
 }
