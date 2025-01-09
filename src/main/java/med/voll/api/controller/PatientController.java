b581826c1ef66e94c9799f51dac02ae8c53bf37d
JKaypa <jose_kaypa@hotmail.com>
2025-01-09 16:04:20 -0500

    imports

--- src/main/java/med/voll/api/controller/PatientController.java
+++ src/main/java/med/voll/api/controller/PatientController.java
@@ -1,18 +1,31 @@
 package med.voll.api.controller;
 
-import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
-import med.voll.api.domain.patient.*;
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.domain.patient.Patient;
+import med.voll.api.domain.patient.PatientDetailDto;
+import med.voll.api.domain.patient.PatientDto;
+import med.voll.api.domain.patient.PatientRepository;
+import med.voll.api.domain.patient.PatientResponseDto;
+import med.voll.api.domain.patient.UpdatePatientDto;
 
 
 @RestController
