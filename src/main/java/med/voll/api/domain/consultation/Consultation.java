b7d7c22a20ebcbd6e9038f2a99e08a2bca56af8a
JKaypa <jose_kaypa@hotmail.com>
2025-01-09 16:09:22 -0500

    entity

---
+++ src/main/java/med/voll/api/domain/consultation/Consultation.java
@@ -0,0 +1,34 @@
+package med.voll.api.domain.consultation;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.domain.doctor.Doctor;
+import med.voll.api.domain.patient.Patient;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode(of = "id")
+@Entity
+public class Consultation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    private LocalDateTime date;
+
+
+}
