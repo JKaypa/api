7a2d5cfde42ca4bafec4bdc623618242b954897c
JKaypa <jose_kaypa@hotmail.com>
2025-01-10 12:24:45 -0500

    add specialty

--- src/main/java/med/voll/api/domain/consultation/AppointmentRequestDto.java
+++ src/main/java/med/voll/api/domain/consultation/AppointmentRequestDto.java
@@ -2,12 +2,14 @@ package med.voll.api.domain.consultation;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.doctor.Specialty;
 
 import java.time.LocalDateTime;
 
 public record AppointmentRequestDto(
-        @NotNull
         Long idDoctor,
+
+        Specialty specialty,
 
         @NotNull
         Long idPatient,
