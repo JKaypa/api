aaa2d400dd86e7022113310680b45f7b1c9efdf8
JKaypa <jose_kaypa@hotmail.com>
2025-01-10 12:25:30 -0500

    add constructor

--- src/main/java/med/voll/api/domain/consultation/AppointmentResponseDto.java
+++ src/main/java/med/voll/api/domain/consultation/AppointmentResponseDto.java
@@ -3,8 +3,11 @@ package med.voll.api.domain.consultation;
 import java.time.LocalDateTime;
 
 public record AppointmentResponseDto(
-        Long id,
-        Long idDoctor,
-        Long idPatient,
-        LocalDateTime date) {
+                Long id,
+                Long idDoctor,
+                Long idPatient,
+                LocalDateTime date) {
+        public AppointmentResponseDto(AppointmentRequestDto appointment) {
+                this(null, appointment.idDoctor(), appointment.idPatient(), appointment.date());
+        }
 }
