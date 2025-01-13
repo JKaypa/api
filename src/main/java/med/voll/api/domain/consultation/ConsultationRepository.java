7b9eabfd6acf911e9a85f349cb5d592fa2e054ff
JKaypa <jose_kaypa@hotmail.com>
2025-01-13 08:31:24 -0500

    exists by patient and doctor

--- src/main/java/med/voll/api/domain/consultation/ConsultationRepository.java
+++ src/main/java/med/voll/api/domain/consultation/ConsultationRepository.java
@@ -1,6 +1,15 @@
 package med.voll.api.domain.consultation;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 public interface ConsultationRepository extends JpaRepository<Consultation, Long> {
+
+  boolean   existsByPatientIdAndDateBetween(Long idPatient, LocalDateTime openingHours,
+      LocalDateTime closingHours);
+
+  boolean existsByDoctorIdAndDate(Long idDoctor, LocalDateTime date);
 }
