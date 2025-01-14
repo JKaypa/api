c3da77ee00317e6257f67afecf7909ba47f9f875
JKaypa <jose_kaypa@hotmail.com>
2025-01-13 20:33:42 -0500

    refine cancel reason enum

--- src/main/java/med/voll/api/domain/consultation/CancelReason.java
+++ src/main/java/med/voll/api/domain/consultation/CancelReason.java
@@ -1,7 +1,31 @@
 package med.voll.api.domain.consultation;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum CancelReason {
-	PATIENT_DESISTED,
-	DOCTOR_DESISTED,
-	OTHER
+	PATIENT_DESISTED("patient desisted"),
+	DOCTOR_DESISTED("doctor desisted"),
+	OTHER("other");
+
+	public final String reason;
+	CancelReason(String reason){
+		this.reason = reason;
+	}
+
+	@Override
+	public String toString() {
+		return reason;
+	}
+
+	@JsonCreator
+	public static CancelReason fromString(String reason){
+		for(CancelReason value : values()){
+			if(value.name().equalsIgnoreCase(reason)){
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Enum reason argument is not correct: " + reason);
+	}
+
+	
 }
