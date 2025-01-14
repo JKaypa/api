a03092f8799f6e27546019bad1ce50035a263be1
JKaypa <jose_kaypa@hotmail.com>
2025-01-13 20:31:06 -0500

    add more errors

--- src/main/java/med/voll/api/infra/errors/ErrorHandler.java
+++ src/main/java/med/voll/api/infra/errors/ErrorHandler.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityNotFoundException;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -18,10 +19,20 @@ public class ErrorHandler {
         return ResponseEntity.notFound().build();
     }
 
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> notFound(NotFoundException error){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error.getMessage());
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<ValidationError>> handleError400(MethodArgumentNotValidException error){
         var errors = error.getFieldErrors().stream().map(ValidationError::new).toList();
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(BusinessRuleException.class)
+    public ResponseEntity<String> businessExceptions(BusinessRuleException error){
+        return ResponseEntity.badRequest().body(error.getMessage());
     }
 
     private record ValidationError(String field, String error){
