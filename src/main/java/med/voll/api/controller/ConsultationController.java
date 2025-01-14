1620b4a93da151f33829e5f888db2c81e6801dd7
JKaypa <jose_kaypa@hotmail.com>
2025-01-13 20:36:58 -0500

    refine cancellation functionality

--- src/main/java/med/voll/api/controller/ConsultationController.java
+++ src/main/java/med/voll/api/controller/ConsultationController.java
@@ -3,6 +3,11 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.domain.consultation.AppointmentRequestDto;
 import med.voll.api.domain.consultation.AppointmentResponseDto;
+import med.voll.api.domain.consultation.CancelAppointmentRequestDto;
+import med.voll.api.domain.consultation.CancelAppointmentResponseDto;
+import med.voll.api.domain.consultation.ConsultationService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,14 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("consultations")
 public class ConsultationController {
 
+    @Autowired
+    private ConsultationService consultationService;
+
     @PostMapping
     public ResponseEntity<AppointmentResponseDto> schedule(@RequestBody @Valid AppointmentRequestDto appointmentRequestDto){
-        System.out.println(appointmentRequestDto);
-        return ResponseEntity.ok(new AppointmentResponseDto(appointmentRequestDto));
+        var consultation = consultationService.bookingConsultation(appointmentRequestDto);
+        return ResponseEntity.ok(consultation);
     }
 
     @DeleteMapping
-    public ResponseEntity<Void> cancelScheduling(){
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<CancelAppointmentResponseDto> cancelScheduling(@RequestBody @Valid CancelAppointmentRequestDto cancellation){
+        var cancelled = consultationService.cancelConsultation(cancellation);
+        return ResponseEntity.ok(cancelled);
     }
 }
