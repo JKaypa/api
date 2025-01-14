f3270d535ca297ca6ff3bdd0ca2de41f879841f0
JKaypa <jose_kaypa@hotmail.com>
2025-01-13 20:36:35 -0500

    refactor

--- src/main/java/med/voll/api/domain/consultation/AppointmentResponseDto.java
+++ src/main/java/med/voll/api/domain/consultation/AppointmentResponseDto.java
@@ -2,12 +2,9 @@ package med.voll.api.domain.consultation;
 
 import java.time.LocalDateTime;
 
-public record AppointmentResponseDto(
-                Long id,
-                Long idDoctor,
-                Long idPatient,
-                LocalDateTime date) {
-        public AppointmentResponseDto(AppointmentRequestDto appointment) {
-                this(null, appointment.idDoctor(), appointment.idPatient(), appointment.date());
+public record AppointmentResponseDto(Long id, Long idDoctor, Long idPatient, LocalDateTime date) {
+        public AppointmentResponseDto(Consultation appointment) {
+                this(appointment.getId(), appointment.getDoctor().getId(),
+                                appointment.getPatient().getId(), appointment.getDate());
         }
 }
